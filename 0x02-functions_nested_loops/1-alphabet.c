06a58157b0fcac215dd23b529f442fb2f1cadc1f
afradZ <afradzakaria@gmail.com>
2024-06-25 10:36:35 -0700

    program that prints the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
  * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Description: this is a function that prints the alphabet
+ * using the _putchar function. Each letter is printed in sequence, followed
  */
 
 void print_alphabet(void)
