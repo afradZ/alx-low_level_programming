f3cd9c4670e1dea29aedd693758752d340e134ec
afradZ <afradzakaria@gmail.com>
2024-07-01 13:14:25 -0700

    function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,33 +4,29 @@
  * _atoi - Converts a string to an integer.
  * @s: The string to be converted.
  *
- * Return: The integer value of the string.
+ * Return: The integer value of the converted string.
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
 	int i = 0;
+	int sign = 1;
+	unsigned int num = 0;
 
-	while (s[i] != '\0')
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
 		{
-			sign = -sign;
-		}
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			break;
+			sign *= -1;
 		}
 		i++;
 	}
 
-	while (s[i] >= '0' && s[i] <= '9')
+	while (s[i] && (s[i] >= '0' && s[i] <= '9'))
 	{
-		result = result * 10 + (s[i] - '0');
+		num = (num * 10) + (s[i] - '0');
 		i++;
 	}
 
-	return (result * sign);
+	return (num * sign);
 }
 
